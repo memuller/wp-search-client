1551742569ef2f66a70ace9c7fe10e0d45acdc60
Matheus Elias Muller <hello@memuller.com>
2011-10-24 06:06:49 -0200

    Functional model skeleton.

--- models/Post.php
+++ models/Post.php
@@ -2,29 +2,43 @@
 	namespace SearchCN ;
 	use Pest as Pest ;
 	class Document {
-		
-		public $id ;
 
-		public $author_email ;
-		public $author_name ;
-		
-		public $title ;
-		public $content ;
-		public $excerpt ;
-		public $comments_count ; 
-		
-		public $category_name ;
-		public $tags ;
+		const server_addr = "http://fedora:3000" ;
+		public $post ; 
+		private $client ; 
 
 		function __construct($id) {
-			$pest = new Pest(get_option('siteurl')) ;
-			$post = get_post($id) ;
+			$this->post = get_post($id) ;
+			if ($this->is_indexable()) {				
+				$this->index();
+			}
+		}
 
-			$this->title = $post->post_title ;
+		private function is_indexable(){
+			return $this->post->post_type == 'post' && $this->post->post_status == 'publish' ;
+		}
 
-			$request = $pest->get('/') ;
+		private function build_client(){
+			$this->client = new Pest(self::server_addr) ;
+		}
 
-			update_option('wpsearch', $request);
+		private function build_post_arguments(){
+			$arguments = array('document[title]' => $this->post->post_title,
+				'document[uri]' => get_permalink($this->id),
+				'document[exerpt]' => $this->post->post_excerpt
+			 );
+			 return $arguments ; 
+		}
+
+		private function index(){
+			$this->build_client();
+			$result = $this->client->post( 
+				'/documents', $this->build_post_arguments() );
+			$this->debug($result);
+		}
+
+		private function debug($message){
+			update_option('search_debug', $message);
 		}
 	}
  ?>
